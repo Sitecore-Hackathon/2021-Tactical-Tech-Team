9aadc7ce9ee4920d644a102cbffa6b9c190bc612
Andrii Shtadler <andrii_shtadler@epam.com>
2021-03-06 23:00:27 +0200

    NUXT: Hero component

--- frontend/components/sitecore/Hero/index.vue
+++ frontend/components/sitecore/Hero/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="box">
-    <h3 v-if="fields.title">
+    <h3 v-if="fields.title" class="title">
       <ScText :field="fields.title" />
     </h3>
-    <p v-if="fields.description">
+    <p v-if="fields.description" class="description">
       <ScRichText :field="fields.description" />
     </p>
-    <ScLink v-if="fields.link" :field="fields.link" />
+    <ScLink v-if="fields.link" :field="fields.link" class="button--grey button-small" />
   </div>
 </template>
 
@@ -29,5 +29,26 @@ export default {
 </script>
 
 <style scoped>
+.box {
+  text-align: left;
+  margin: 25px 25px;
+  padding: 15px 20px;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+}
+.title {
+  color: #35495e;
+  font-size: 28px;
 
+}
+
+.description {
+  color: #526488;
+  margin-top: 5px;
+}
+
+.button-small {
+  border-color: goldenrod;
+  color: goldenrod;
+  margin: 15px 0;
+}
 </style>
--- frontend/env.js
+++ frontend/env.js
@@ -4,5 +4,5 @@ export const ENV = {
   sitecoreCDHost: 'https://cm.ttt.localhost',
   disconnectedHost: 'http://localhost:3000/api',
   jssKey: '{01BD4964-AE28-402D-9EE1-D5F1E32881B2}',
-  isDisconnected: true
+  isDisconnected: false
 }
